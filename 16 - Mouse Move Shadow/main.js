6bb6f23c363109e7659d6f64f8bbb70ce1db345d
downlouder <118766413+downlouder@users.noreply.github.com>
2023-03-06 19:42:51 +0200

    Completed 16, 17, 18

---
+++ 16 - Mouse Move Shadow/main.js
@@ -0,0 +1,27 @@
+const hero = document.querySelector('.hero');
+const text = document.querySelector('h1');
+const walk = 500;
+
+function shadow(e) {
+    const { offsetWidth: width, offsetHeight: height } = hero;
+    let { offsetX: x, offsetY: y } = e;
+
+    if(this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / width * height) - (walk / 2));
+
+    text.style.textShadow = `
+        ${xWalk}px ${yWalk}px 0 red,
+        ${xWalk * -1}px ${yWalk}px 0 aqua,
+        ${yWalk * -1}px ${xWalk}px 0 purple,
+        ${yWalk}px ${xWalk * -1}px 0 orange
+    `;
+
+    console.log(x, y)
+}
+
+hero.addEventListener('mousemove', shadow)
